ae6e626a21d0bd38ca14e58b1e645cf8421960a7
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-01 13:37:44 +0300

    Add favicon depends on color schema dark/light which set up on user pc

--- src/app/components/landing/footer/footer.component.ts
+++ src/app/components/landing/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ScrollToService } from 'src/shared/services/scroll-to.service';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
 
+    constructor(private scrollTo: ScrollToService) {}
+
+    public onClickLi(selector: string, speed: number = 500): void {
+        this.scrollTo.scroll(selector, speed);
+    }
 }
--- src/app/components/landing/header/mobile-header/mobile-header.component.ts
+++ src/app/components/landing/header/mobile-header/mobile-header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-mobile-header',
-  templateUrl: './mobile-header.component.html',
-  styleUrls: ['./mobile-header.component.scss']
+    selector: 'app-mobile-header',
+    templateUrl: './mobile-header.component.html',
+    styleUrls: ['./mobile-header.component.scss']
 })
 export class MobileHeaderComponent {
 
+    @Output() onClickList: EventEmitter<{selector: string, speed: number}> = new EventEmitter();
+    @Input() isSticky: boolean = false;
+
+    public isOpenMenu: boolean = false;
+
+    public onClickLi(selector: string, speed: number = 1000): void {
+        this.onClickList.emit({selector, speed});
+    }
+
+    public toggleMenu(): void {
+        this.isOpenMenu = !this.isOpenMenu;
+    }
+
+    
 }
