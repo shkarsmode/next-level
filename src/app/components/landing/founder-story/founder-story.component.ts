e633967981da5c693a0264af82a4df17c8e09152
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-01 15:49:20 +0300

    Add footer and contact us adaptive

--- src/app/components/landing/founder-story/founder-story.component.ts
+++ src/app/components/landing/founder-story/founder-story.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-founder-story',
-  templateUrl: './founder-story.component.html',
-  styleUrls: ['./founder-story.component.scss']
+    selector: 'app-founder-story',
+    templateUrl: './founder-story.component.html',
+    styleUrls: ['./founder-story.component.scss']
 })
 export class FounderStoryComponent {
 
+    public isShowMore: boolean = false;
+
+    public toggleList(): void {
+        this.isShowMore = !this.isShowMore;
+    }
 }
