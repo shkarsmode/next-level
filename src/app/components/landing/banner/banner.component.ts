186fa7753a22c99f180dc12db2116957bb1ff539
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-01 13:39:33 +0300

    Add scroll to contact after clicking on banner button

--- src/app/components/landing/banner/banner.component.ts
+++ src/app/components/landing/banner/banner.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ScrollToService } from 'src/shared/services/scroll-to.service';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
 
+    constructor(
+        private scrollToService: ScrollToService
+    ) {}
+
+    public scrollTo(selector: string): void {
+        this.scrollToService.scroll(selector);
+    }
 }
