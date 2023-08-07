67c17f7b52942d791145a119cdba4809b188cc62
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-07 17:48:41 +0300

    Update git ignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	production: true,
+	basePathApi: 'https://next-level-back.vercel.app',
+	geoPathAPI: 'https://api.geoapify.com/v1/geocode/autocomplete',
+	geoApiKey: 'c10f5f77fe91459fb83c2dce469ce036'
+};
