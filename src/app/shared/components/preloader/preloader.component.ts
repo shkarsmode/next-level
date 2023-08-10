75668b3b140abdb4e831d33e10ebfdd824bb6c94
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-10 14:41:40 +0300

    Update preloader

--- src/app/shared/components/preloader/preloader.component.ts
+++ src/app/shared/components/preloader/preloader.component.ts
@@ -10,14 +10,16 @@ export class PreloaderComponent {
     @Input() isLoading: boolean = true;
 
     public ngOnInit(): void {
+        this.removePreloaderForDevelopment();
+    }
+
+    private removePreloaderForDevelopment(): void {
         const isNotShowPreloader = !!localStorage.getItem('isNotShowPreloader');
         if (isNotShowPreloader) {
             this.isLoading = false;
             return;
         }
 
-        setTimeout(() => {
-            this.isLoading = false;
-        }, 2700);
+        setTimeout(() => this.isLoading = false, 2700);
     }
 }
