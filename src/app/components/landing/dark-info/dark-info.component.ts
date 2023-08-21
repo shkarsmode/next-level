58cf285b377cb7ec928a4316e8112110a7d3b8fc
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-21 14:00:36 +0300

    Update dark-grey-info to dark-info

---
+++ src/app/components/landing/dark-info/dark-info.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-dark-info',
+    templateUrl: './dark-info.component.html',
+    styleUrls: ['./dark-info.component.scss']
+})
+export class DarkInfoComponent {
+
+    @Input() public info: any;
+}
