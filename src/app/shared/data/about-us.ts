9788dbc6730e7c3cb309d28f144814a5f19aa0ae
Shkarupa Daniil <zshkarrr@outlook.com>
2023-08-21 11:27:03 +0300

    Update capitalizing of different blocks + change aligning of the white blocks

--- src/app/shared/data/about-us.ts
+++ src/app/shared/data/about-us.ts
@@ -26,15 +26,11 @@ export const aboutUsInfo = {
 	],
 	white: [
 		{
-			title: 'Сlose relationships',
+			title: 'Сlose Relationships',
 			description: 'We envision the Next Level Oncology concierge practice as a community of real people and real families with stories, dreams and wishes that deserve to be heard, and with complex and demanding lives and illnesses like cancer that deserve the utmost attention to detail and respect. <br><br> When  our patients call, we answer, and when they feel sick or worried or simply have questions we make time for them. We may travel to their homes when needed and can be with them through the hardest and most difficult times or simply just to listen in the comfort of one’s home surrounded by their loved ones. The cancer journey is unique and complicated but there are still many opportunities for celebration. Your concierge doctor will be with you as much in celebration as in illness. It is a powerful form of practice, rooted in the community relationships of the Old World and dedicated to a deeply personal vision of cancer care and healthcare in general—one that is too often lost in the medical landscape of today.'
 		},
 		{
-			title: 'Patient-first approach',
-			description: 'Our mission is to change the shortcomings of current oncology practices completely. At Next Level Oncology, we’re dedicated to providing better and happier lives by building deeper, more lasting connections to our patients. We combine our patient-first values with the best of personalized diagnostics, preventative care and treatments both supportive and therapeutic to treat the person as a whole, and to help our patients achieve their fullest and best lives possible. To make that possible, we’ve committed ourselves to getting to know you, because we believe that you deserve more than the highest quality of care. We believe that you deserve exceptional care on your own terms, based on your needs, just as you are. That’s our promise and we hold ourselves to it.'
-		},
-		{
-			title: 'Patient-first approach',
+			title: 'Patient-First Approach',
 			description: 'Our mission is to change the shortcomings of current oncology practices completely. At Next Level Oncology, we’re dedicated to providing better and happier lives by building deeper, more lasting connections to our patients. We combine our patient-first values with the best of personalized diagnostics, preventative care and treatments both supportive and therapeutic to treat the person as a whole, and to help our patients achieve their fullest and best lives possible. To make that possible, we’ve committed ourselves to getting to know you, because we believe that you deserve more than the highest quality of care. We believe that you deserve exceptional care on your own terms, based on your needs, just as you are. That’s our promise and we hold ourselves to it.'
 		}
 	]
