426c111c914ac322d1d61235aef553e23c78ad05
Daniil Shkarupa <zshkarrr@gmail.com>
2024-03-21 03:32:44 +0200

    hide foundations layout

--- src/app/layouts/landing-layout/landing-layout.module.ts
+++ src/app/layouts/landing-layout/landing-layout.module.ts
@@ -4,7 +4,7 @@ import { LandingModule } from 'src/app/components/landing/landing.module';
 import { SharedComponentsModule } from 'src/app/components/shared/shared-components.module';
 import { AboutUsLayoutComponent } from './components/about-us-layout/about-us-layout.component';
 import { CompassionateLayoutComponent } from './components/compassionate-layout/compassionate-layout.component';
-import { FounderLayoutComponent } from './components/founder-layout/founder-layout.component';
+// import { FounderLayoutComponent } from './components/founder-layout/founder-layout.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { LandingRoutingModule } from './landing-layout-routing.module';
 import { LandingLayoutComponent } from './landing-layout.component';
@@ -14,7 +14,7 @@ import { LandingLayoutComponent } from './landing-layout.component';
         LandingLayoutComponent,
         MainLayoutComponent, 
         AboutUsLayoutComponent, 
-        FounderLayoutComponent, 
+        // FounderLayoutComponent, 
         CompassionateLayoutComponent
     ],
     imports: [
